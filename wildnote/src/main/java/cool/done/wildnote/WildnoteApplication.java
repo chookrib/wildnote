f849149b41b26809ce313024246e22f588893db1
chookrib <chookrib@gmail.com>
2024-10-28 14:34:20 +0800

    refactor

---
+++ wildnote/src/main/java/cool/done/wildnote/WildnoteApplication.java
@@ -0,0 +1,13 @@
+package cool.done.wildnote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WildnoteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WildnoteApplication.class, args);
+    }
+
+}
---
+++ wildnote/src/test/java/cool/done/wildnote/WildnoteApplicationTests.java
@@ -0,0 +1,13 @@
+package cool.done.wildnote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WildnoteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
