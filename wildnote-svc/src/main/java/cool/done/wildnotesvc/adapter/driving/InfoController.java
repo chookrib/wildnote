9849de3c9f15c37f5ead7ddd8cae87821a8cce70
chookrib <chookrib@gmail.com>
2024-12-11 13:21:54 +0800

    Changes

--- wildnote-svc/src/main/java/cool/done/wildnotesvc/adapter/driving/InfoController.java
+++ wildnote-svc/src/main/java/cool/done/wildnotesvc/adapter/driving/InfoController.java
@@ -17,12 +17,10 @@ public class InfoController {
     @RequestMapping(value = "/info", method = RequestMethod.GET, produces = "text/plain")
     @ResponseBody
     public String  info() {
-        StringBuilder sb = new StringBuilder();
-        sb.append("File-Name: ").append(WildnoteSvcApplication.getFileName());
-        sb.append(System.lineSeparator());
-        sb.append("Build-Time: ").append(WildnoteSvcApplication.getBuildTime());
-        sb.append(System.lineSeparator());
-        return sb.toString();
+        return "File-Name: " + WildnoteSvcApplication.getFileName() +
+                System.lineSeparator() +
+                "Build-Time: " + WildnoteSvcApplication.getBuildTime() +
+                System.lineSeparator();
     }
 }
 
