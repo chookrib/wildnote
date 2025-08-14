c1b9fede744a7e3c04a23fb3312ad2d41682fe85
chookrib <chookrib@gmail.com>
2025-08-15 03:55:12 +0800

    Changes

--- wildnote-run.bat
+++ wildnote-run.bat
@@ -29,9 +29,9 @@ cd /d "%~dp0"
 set params=%*
 rem set params=%1 %2 %3 %4 %5 %6 %7 %8 %9
 
-echo %PATH%
-git --version
-echo %cd%
+rem echo %PATH%
+rem git --version
+rem echo %cd%
 
 git checkout main
 git pull
