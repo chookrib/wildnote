056c9056e70d87548a26a95d7c905d2c1ba9e78f
chookrib <chookrib@gmail.com>
2025-11-15 18:06:08 +0800

    Changes

--- wildnote-web-ts/vite.config.ts
+++ wildnote-web-ts/vite.config.ts
@@ -5,6 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 import Components from 'unplugin-vue-components/vite';
 import {AntDesignVueResolver} from 'unplugin-vue-components/resolvers';
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -27,8 +28,10 @@ export default defineConfig({
     build: {
         rollupOptions: {
             input: {
-                index: fileURLToPath(new URL('./index.html', import.meta.url)),
-                login: fileURLToPath(new URL('./login.html', import.meta.url)),
+                // index: fileURLToPath(new URL('./index.html', import.meta.url)),
+                // login: fileURLToPath(new URL('./login.html', import.meta.url)),
+                index: path.resolve(__dirname, 'index.html'),
+                login: path.resolve(__dirname, 'login.html'),
             }
         }
     },
