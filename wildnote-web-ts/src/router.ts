4b7a5b1b0c9ce70f45eb38ca87bfeccc47ffd8b6
chookrib <chookrib@gmail.com>
2025-12-02 11:46:02 +0800

    Changes

--- wildnote-web-ts/src/router.ts
+++ wildnote-web-ts/src/router.ts
@@ -23,31 +23,31 @@ const router = createRouter({
       component: () => import('@/views/home/Layout.vue'),
       children: [
         {
-          path: '/index',
+          path: 'index',
           component: () => import('@/views/home/Index.vue'),
         },
         {
-          path: '/explore',
+          path: 'explore',
           component: () => import('@/views/home/Explore.vue'),
         },
         {
-          path: '/search',
+          path: 'search',
           component: () => import('@/views/home/Search.vue'),
         },
         {
-          path: '/remind',
+          path: 'remind',
           component: () => import('@/views/home/Remind.vue'),
         },
         {
-          path: '/note',
+          path: 'note',
           component: () => import('@/views/home/Note.vue'),
         },
         {
-          path: '/system',
+          path: 'system',
           component: () => import('@/views/home/System.vue'),
         },
         {
-          path: '/log',
+          path: 'log',
           component: () => import('@/views/home/Log.vue'),
         },
       ],
